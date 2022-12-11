814637ad1080243faaac834ad7d0569c2390efc1
Malte Siemers <malte.siemers@mailbox.org>
2022-12-11 19:03:20 +0100

    add windows install script

---
+++ bridge.bat
@@ -0,0 +1,13 @@
+@echo off
+
+if exist python3env\ (
+  echo using existing python3env...
+) else (
+  python3 -m venv python3env
+  call python3env\Scripts\activate
+  pip install --no-cache-dir --upgrade pip
+  pip install --no-cache-dir -r requirements.txt
+  deactivate
+)
+
+python3env\Scripts\python.exe bridge.py
